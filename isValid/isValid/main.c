0338b261d1ce71c3f10e6532df9eb2bf61137e96
Coolsuperman <846069147@qq.com>
2019-03-13 22:11:38 +0800

    有效括号-新增栈算法

--- isValid/isValid/main.c
+++ isValid/isValid/main.c
@@ -1,7 +1,62 @@
 //给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。有效字符串需满足：左括号必须用相同类型的右括号闭合。左括号必须以正确的顺序闭合。注意空字符串可被认为是有效字符串。
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<stdlib.h>
+typedef struct Stack {
+	int array[10000];
+	int top;
+}Stack;
 
+void stackInit(Stack*stack) {
+	stack->top = 0;
+}
+int stackEmpty(Stack*stack) {
+	return stack->top == 0 ? 1 : 0;
+}
+void stackPush(Stack*stack, int value) {
+	stack->array[stack->top] = value;
+	stack->top++;
+}
+void stackPop(Stack*stack) {
+	stack->top--;
+}
+int stackTop(Stack*stack) {
+	return stack->array[stack->top - 1];
+}
+int isValid2(char* s)
+{
+	Stack stack;
+	stackInit(&stack);
+	while (*s) {
+		switch (*s) {
+		case'{':
+		case'[':
+		case'(':
+			stackPush(&stack, (int)*s);
+			break;
+		case'}':
+		case')':
+		case']': {
+			if (stackEmpty(&stack)) {
+				return 0;
+			}
+			char left = (char)stackTop(&stack);
+			stackPop(&stack);
+			if (!(left + 2 == *s || left + 1 == *s)) {
+				return 0;
+			}
+			break;
+		}
+		default:
+			break;
+		}
+		s++;
+	}
+	if (!stackEmpty(&stack)) {
+		return 0;
+	}
+	return 1;
+}
 int isValid(char* s)
 {
 	int len = 0;
